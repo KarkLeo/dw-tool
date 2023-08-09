405dd876f7fab6ab09bb01607dad6fb23aa48bc0
Viacheslav Malykhin <vyacheslav.malykhin@clockwise.software>
2023-08-09 14:03:25 +0300

    Add main menu

---
+++ packages/dw-tool-frontend/src/common/Menu/Menu.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import { PATHS } from '../../routes/config'
+
+const tStyles = {
+  container: 'flex space-x-4 bg-blue-500 text-white p-4',
+  link: 'hover:bg-blue-700 px-2 py-1 rounded',
+}
+
+export const Menu = () => {
+  return (
+    <nav className={tStyles.container}>
+      <li>
+        <Link to={PATHS.APP} className={tStyles.link}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to={PATHS.GAMES} className={tStyles.link}>
+          Games
+        </Link>
+      </li>
+      <li>
+        <Link to={PATHS.GAMES_CREATE} className={tStyles.link}>
+          Create game
+        </Link>
+      </li>
+    </nav>
+  )
+}
