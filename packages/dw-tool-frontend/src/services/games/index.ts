6bb9a2a9078521b98b5824dac8a0256798806907
Viacheslav Malykhin <vyacheslav.malykhin@clockwise.software>
2023-08-14 22:59:55 +0300

    Game services

--- packages/dw-tool-frontend/src/services/games/index.ts
+++ packages/dw-tool-frontend/src/services/games/index.ts
@@ -1,8 +1,19 @@
 import instance from '../instance'
-import { ICreateGameRequest } from './types'
+import {
+  ICreateGameRequest,
+  IGameAddPlayerCharacterRequest,
+  IGameChangePlayerStatusRequest,
+  IGameUpdateStatusRequest,
+} from './types'
 
 export const gamesServices = {
   create: (data: ICreateGameRequest) => instance.post('games/create', data),
   findAll: () => instance.get('games/'),
   findOne: (id: number | string) => instance.get(`games/${id}`),
+  updatePlayerStatus: (data: IGameChangePlayerStatusRequest) =>
+    instance.post('games/update-player-status', data),
+  addPlayerCharacter: (data: IGameAddPlayerCharacterRequest) =>
+    instance.post('games/add-player-character', data),
+  updateGameStatus: (data: IGameUpdateStatusRequest) =>
+    instance.post('games/update-game-status', data),
 }
--- packages/dw-tool-frontend/src/services/games/types.ts
+++ packages/dw-tool-frontend/src/services/games/types.ts
@@ -5,3 +5,34 @@ export interface ICreateGameRequest {
     id: number
   }[]
 }
+
+export interface IGameChangePlayerStatusRequest {
+  playerId: number
+  status: PlayerStatusesEnum
+}
+
+export interface IGameAddPlayerCharacterRequest {
+  playerId: number
+  characterId: number
+}
+
+export interface IGameUpdateStatusRequest {
+  gameId: number
+  status: GameStatusesEnum
+}
+
+export enum GameStatusesEnum {
+  PENDING = 'PENDING',
+  READY = 'READY',
+  IN_PROGRESS = 'IN_PROGRESS',
+  FINISHED = 'FINISHED',
+  PAUSED = 'PAUSED',
+}
+
+export enum PlayerStatusesEnum {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+  KICKED = 'KICKED',
+  LEFT = 'LEFT',
+}
