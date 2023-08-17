8a08c9e2f699dfe76b0b080f304df0f2835dceee
Viacheslav Malykhin <vyacheslav.malykhin@clockwise.software>
2023-08-17 18:26:53 +0300

    [Back-End] Fixed Meta

--- packages/dw-tool-backend/src/ormconfig.ts
+++ packages/dw-tool-backend/src/ormconfig.ts
@@ -1,5 +1,24 @@
 import { ConfigService } from '@nestjs/config'
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as process from 'process'
+import { DataSource, DataSourceOptions } from 'typeorm'
+import 'dotenv/config'
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  synchronize: false,
+}
+
+const dataSource = new DataSource(dataSourceOptions)
+
+export default dataSource
 
 export const createOrmConfig = (
   configService: ConfigService,
